76078109f6051a871ca2b737fae99bdf3a6e6f45
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-10-31 16:34:09 +0000

    Fix pyforms imports

--- pythonvideoannotator_module_pathmap/module.py
+++ pythonvideoannotator_module_pathmap/module.py
@@ -1,17 +1,17 @@
 import cv2
-from pysettings import conf
+from confapp import conf
 from pythonvideoannotator_module_pathmap.pathmap_window import PathMapWindow
 
 
 class Module(object):
 
-	def __init__(self):
-		"""
-		This implements the Path edition functionality
-		"""
-		super(Module, self).__init__()
-		self.pathmap_window = PathMapWindow(self)
+    def __init__(self):
+        """
+        This implements the Path edition functionality
+        """
+        super(Module, self).__init__()
+        self.pathmap_window = PathMapWindow(self)
 
-		self.mainmenu[1]['Modules'].append(
-			{'Path map': self.pathmap_window.show, 'icon':conf.ANNOTATOR_ICON_PATHMAP },			
-		)
+        self.mainmenu[1]['Modules'].append(
+            {'Path map': self.pathmap_window.show, 'icon':conf.ANNOTATOR_ICON_PATHMAP },            
+        )
